c942747bf5600bbc7a37d70747e91a3c52182db8
gjsanchez26 <gjsanchez26@gmail.com>
2017-11-20 15:56:51 -0600

    botones

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,88 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  response: Observable<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,  public http: Http) {
 
   }
+ 
+ select():void{
+    console.log("Select");
+    this.response = this.http.get("http://192.168.0.33:40314/select");
+    this.response
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    })
+    
+ }
 
-}
+ start():void{
+    console.log("Start");
+    this.response = this.http.get();
+    this.response
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    })
+    
+ }
+  pressedLeft():void{
+    console.log("<-");
+    this.response = this.http.get( "http://192.168.0.33:40314/left");
+    //console.log(this.response);
+    this.response
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    })
+    
+  
+  }
+  pressedRight():void{
+    console.log("->");
+    this.response = this.http.get( "http://192.168.0.33:40314/right");
+    //console.log(this.response);
+    this.response
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    })
+   }
+  pressedUp():void{
+    console.log("^");
+    this.response = this.http.get( "http://192.168.0.33:40314/up");
+    //console.log(this.response);
+    this.response
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    })
+    
+  
+  }
+  pressedDown():void{
+    console.log("v");
+    this.response = this.http.get("http://192.168.0.33:40314/down");
+    //console.log(this.response);
+    this.response
+    .map(res => res.json())
+    .subscribe(data => {
+      console.log('my data: ', data);
+    })
+    
+  
+  }
+  
+  }
+
+
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +10,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
+ 
+  }
+  connect():void{
+    console.log("Conneting with "+ this.ip+ " on port "+this.port);
+    
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    
+    let body = {
+        ip : this.ip,
+        port: this.port
+    };
+    
+    this.http.post('https://some.domain',JSON.stringify(body), {headers:headers})
+      .map(res =>res.json())
+      .subscribe(data =>{
+        console.log(data);
+    })
 
   }
 
